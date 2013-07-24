216b3a4508316c78e210ba979b94854e326a79af
jasonkneen <jason.kneen@bouncingfish.com>
2013-07-24 13:54:16 +0200

    commenting

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,3 +1,5 @@
+
+// add our main event listener
 Ti.Gesture.addEventListener("orientationchange", applyOrientiation);
 
 if ( typeof (__parentSymbol.close) == "function") {
@@ -10,34 +12,46 @@ function applyOrientiation() {
 
     function updateControls(children) {
 
+        // defaults
         var orientName = "portrait", lastOrientName = "landscape";
 
+
+        // override defaults based on orientation
         if ((Ti.Gesture.orientation == Ti.UI.LANDSCAPE_LEFT) || (Ti.Gesture.orientation == Ti.UI.LANDSCAPE_RIGHT)) {
             orientName = "landscape";
             lastOrientName = "portrait";
         }
 
+        // Iterate controls
         if (children) {
             children.forEach(function(control) {
 
+                // nullify any properties we just set/used and 
+                // delete them
                 for (var property in control[lastOrientName]) {
                     control[property] = null;
+                    delete control[property];
                 }
 
+                // apply the new orientation
                 if (control[orientName]) {
                     control.applyProperties(control[orientName]);
                 }
-
+                
+                // think about the children!
                 updateControls(control.views || control.children);
             });
         }
     }
-
+    
+    // initial call, wrap in an array
     updateControls([__parentSymbol]);
 }
 
+// init
 applyOrientiation();
 
+// manual function if needed to refresh
 exports.refresh = function() {
     applyOrientiation();
 }
