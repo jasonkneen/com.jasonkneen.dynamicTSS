0bb2975f0b42bb3daf715bfd3a093b3a381bbef0
jasonkneen <jason.kneen@bouncingfish.com>
2013-09-26 09:24:16 +0100

    Tweaked the way properties are applied to work more like Alloy 1.2 dynamic styling.

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,51 +1,52 @@
-
 // add our main event listener
 Ti.Gesture.addEventListener("orientationchange", applyOrientiation);
 
 if ( typeof (__parentSymbol.close) == "function") {
-    __parentSymbol.addEventListener('close', function() {
-        Ti.Gesture.removeEventListener("orientationchange", applyOrientiation);
-    });
+	__parentSymbol.addEventListener('close', function() {
+		Ti.Gesture.removeEventListener("orientationchange", applyOrientiation);
+	});
 }
 
 function applyOrientiation() {
 
-    function updateControls(children) {
+	function updateControls(children) {
 
-        // defaults
-        var orientName = "portrait", lastOrientName = "landscape";
+		// defaults
+		var orientName = "portrait", lastOrientName = "landscape";
 
+		// override defaults based on orientation
+		if ((Ti.Gesture.orientation == Ti.UI.LANDSCAPE_LEFT) || (Ti.Gesture.orientation == Ti.UI.LANDSCAPE_RIGHT)) {
+			orientName = "landscape";
+			lastOrientName = "portrait";
+		}
 
-        // override defaults based on orientation
-        if ((Ti.Gesture.orientation == Ti.UI.LANDSCAPE_LEFT) || (Ti.Gesture.orientation == Ti.UI.LANDSCAPE_RIGHT)) {
-            orientName = "landscape";
-            lastOrientName = "portrait";
-        }
+		// Iterate controls
+		if (children) {
+			children.forEach(function(control) {
 
-        // Iterate controls
-        if (children) {
-            children.forEach(function(control) {
+				// nullify any properties we just set/used and
+				// delete them
+				for (var property in control[lastOrientName]) {				
+					control[property] = null;
+					delete control[property];
+				}
 
-                // nullify any properties we just set/used and 
-                // delete them
-                for (var property in control[lastOrientName]) {
-                    control[property] = null;
-                    delete control[property];
-                }
+				var prop = {};
 
-                // apply the new orientation
-                if (control[orientName]) {
-                    control.applyProperties(control[orientName]);
-                }
-                
-                // think about the children!
-                updateControls(control.views || control.children);
-            });
-        }
-    }
-    
-    // initial call, wrap in an array
-    updateControls([__parentSymbol]);
+				for (var property in control[orientName]) {
+					prop[property] = control[orientName][property];
+				}
+
+				control.applyProperties(prop);
+				
+				// think about the children!
+				updateControls(control.views || control.children);
+			});
+		}
+	}
+
+	// initial call, wrap in an array
+	updateControls([__parentSymbol]);
 }
 
 // init
@@ -53,5 +54,5 @@ applyOrientiation();
 
 // manual function if needed to refresh
 exports.refresh = function() {
-    applyOrientiation();
+	applyOrientiation();
 }
